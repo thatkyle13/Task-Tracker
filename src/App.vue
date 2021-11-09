acf8a851f6335dc6454b4ad2b63f900fd14288b3
Kyle <kylewilfredc@gmail.com>
2021-11-09 18:06:10 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+<NavBar />
   <router-view/>
 </template>
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  components:{ NavBar }
+}
+</script>
 
 <style>
+body {
+  background: #f2f2f2;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
